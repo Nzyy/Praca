91103f373614528a59e14e27483aa36f844a6463
Nzyy <68493396+Nzyy@users.noreply.github.com>
2022-05-09 19:18:03 +0200

    Update BMI.js

--- script/BMI.js
+++ script/BMI.js
@@ -1,1 +1,10 @@
+function bmi() {
+document.getElementById("bmi").innerHTML=""
 
+var Wzrost = document.getElementById("wzrost").value
+var Masa = document.getElementById("masa").value
+
+var BMI = Masa / Math.pow(Wzrost, 2)
+
+document.getElementById("bmi").innerHTML=BMI
+}
