5f2f1bd6c21bdc645fd6ed3dd971bb5de1b3b0db
Nzyy <68493396+Nzyy@users.noreply.github.com>
2022-05-11 21:22:19 +0200

    Update BMI.js

--- script/BMI.js
+++ script/BMI.js
@@ -4,17 +4,28 @@ document.getElementById("bmi").innerHTML=""
 var Wzrost = document.getElementById("wzrost").value
 var Masa = document.getElementById("masa").value
 
-var BMI = Masa / Math.pow(Wzrost, 2)
-
-document.getElementById("bmi").innerHTML="Twoje BMI wynosi: " + BMI.toFixed(1)
-
-if (BMI <= 18.4) {
-  document.getElementById("bmi").innerHTML+=" - niedowaga"
-} else if (BMI <= 24.9) {
-  document.getElementById("bmi").innerHTML+=" - norma"
-} else if (BMI <= 29.9) {
-  document.getElementById("bmi").innerHTML+=" - nadwaga"
-} else if (BMI >= 30) {
-  document.getElementById("bmi").innerHTML+=" - otyłość"
+if (Wzrost == "") {
+  if (Masa == "") {
+    document.getElementById("bmi").innerHTML="Niepoprawnie podano wzrost oraz wagę"
+} else {
+  document.getElementById("bmi").innerHTML="Niepoprawnie podano wzrost"
 }
+
+} else {
+  if (Masa == "") {
+    document.getElementById("bmi").innerHTML="Niepoprawnie podano wagę"
+  } else {
+    var BMI = Masa / Math.pow(Wzrost, 2)
+
+    if (BMI <= 18.4) {
+      document.getElementById("bmi").innerHTML="Twoje BMI wynosi: " + BMI.toFixed(2) + " - niedowaga"
+    } else if (BMI <= 24.9) {
+      document.getElementById("bmi").innerHTML="Twoje BMI wynosi: " + BMI.toFixed(2) + " - norma"
+    } else if (BMI <= 29.9) {
+      document.getElementById("bmi").innerHTML="Twoje BMI wynosi: " + BMI.toFixed(2) + " - nadwaga"
+    } else if (BMI >= 30) {
+      document.getElementById("bmi").innerHTML="Twoje BMI wynosi: " + BMI.toFixed(2) + " - otyłość"
+    }
+    }
+  }
 }
