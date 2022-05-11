1fcd29f4eedca17316b880778b28899678fcda68
Nzyy <68493396+Nzyy@users.noreply.github.com>
2022-05-11 20:37:33 +0200

    Update BMI.js

--- script/BMI.js
+++ script/BMI.js
@@ -6,5 +6,15 @@ var Masa = document.getElementById("masa").value
 
 var BMI = Masa / Math.pow(Wzrost, 2)
 
-document.getElementById("bmi").innerHTML=BMI
+document.getElementById("bmi").innerHTML="Twoje BMI wynosi: " + BMI.toFixed(1)
+
+if (BMI <= 18.4) {
+  document.getElementById("bmi").innerHTML+=" - niedowaga"
+} else if (BMI <= 24.9) {
+  document.getElementById("bmi").innerHTML+=" - norma"
+} else if (BMI <= 29.9) {
+  document.getElementById("bmi").innerHTML+=" - nadwaga"
+} else if (BMI >= 30) {
+  document.getElementById("bmi").innerHTML+=" - otyłość"
+}
 }
