8a030cd986b832ed101a0f401a025b31912c5c98
Nzyy <68493396+Nzyy@users.noreply.github.com>
2022-05-12 08:03:51 +0200

    Update Losowanie.js

--- script/Losowanie.js
+++ script/Losowanie.js
@@ -1,8 +1,5 @@
 function losowanie() {
 var od = document.getElementById("od").value
-var doo = document.getElementById("do").value
-  
-for (od; od <= doo; od++) {
-  document.write(od)
+alert(od)
 
 }
