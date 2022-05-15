1bc3982ca86351597573632e4505623b2aa06ea6
Nzyy <68493396+Nzyy@users.noreply.github.com>
2022-05-15 17:17:35 +0200

    Update Losowanie.js

--- script/Losowanie.js
+++ script/Losowanie.js
@@ -1,8 +1,8 @@
 function losowanie() {
 const Tab = []
 
-var start = document.getElementById("od").value
-var stop = document.getElementById("do").value
+let start = parseInt(document.getElementById("od").value)
+let stop = parseInt(document.getElementById("do").value)
 
 if (start == "") {
   if (stop == "") {
@@ -29,12 +29,17 @@ if (start == "") {
     } else if (stop <= 0) {
       alert("Zmień wartość 'Do:' na > 0")
     } else {
+      if (start > stop) {
+        var a = stop
+        stop += start - stop
+        start += a - start
+      }
       let uwu = stop - start + 1
-      var WL = Math.floor(Math.random() * uwu)
+      let WL = Math.floor(Math.random() * uwu)
       for (start; start<=stop; start++) {
         Tab.push(start)
       }
-        for (var a = 1; a <= 10; a++) {
+        for (let a = 1; a <= 10; a++) {
         if (document.getElementById(a).innerHTML != "b/d") {
         } else {
           document.getElementById(a).innerHTML=Tab[parseInt(WL)]
